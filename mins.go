31a8cd10fc9ef82a03ab93b668f0651edb8d539c
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-29 00:51:28 +0100

    user function

---
+++ mins.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt" 
+
+func main() {
+
+    fmt.Printf( "\x1bc\x1b[42;30m\n %d\n",mins(40,20))
+
+}
+
+func mins( a int, b int) int{
+    if (a<b){
+        
+    return a
+    }
+    return b
+}
