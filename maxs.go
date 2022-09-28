9f12a06c41f426facbca7d6b66bade18693e9960
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-29 00:47:51 +0100

    user function

--- maxs.go
+++ maxs.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 
-    fmt.Printf( "\x1bc\x1b[42;30m\n %d\n",mins(40,20))
+    fmt.Printf( "\x1bc\x1b[42;30m\n %d\n",maxs(40,20))
 
 }
 
-func mins( a int, b int) int{
+func maxs( a int, b int) int{
     if (a>b){
         
     return a
